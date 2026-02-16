c425d432fb2917e4195ad238ebee94f0f836d743
Anxi <ab@tpr.wales>
2026-02-17 01:23:27 +0800

    Initial Commit

---
+++ DateDrop3/DateDrop3App.swift
@@ -0,0 +1,17 @@
+//
+//  DateDrop3App.swift
+//  DateDrop3
+//
+//  Created by mac on 2026/2/17.
+//
+
+import SwiftUI
+
+@main
+struct DateDrop3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
